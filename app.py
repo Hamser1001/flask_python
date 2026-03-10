cd49ac25f605d0e6f6f1a6b788ec0b7f52319060
Hamser1001 <upscale.mind50@gmail.com>
2026-03-10 02:03:12 +0000

    update

--- app.py
+++ app.py
@@ -7,9 +7,26 @@ from models import db, Book, Author, Review
 # module for working with file paths
 import os
 
+# module for background tasks, celery
+from celery import Celery, Task
+
 
 # create Flask application
 app = Flask(__name__)
+
+
+def celery_init_app(app: Flask) -> Celery:
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
+
 
 # get the absolute path of the current project directory
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
