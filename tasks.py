184ac3727f8abd3cbc8706a9f06529007cb505e9
Hamser1001 <upscale.mind50@gmail.com>
2026-03-10 02:43:48 +0000

    try to make the notification slow

--- tasks.py
+++ tasks.py
@@ -1,8 +1,11 @@
 import time
 from app import celery_app
 
+
 @celery_app.task
 def send_book_notification(book_id, title):
     print(f"Starting notification task for book {book_id}:{title}")
-    time.sleep(5)
+    for x in range(20):
+        time.sleep(5)
+        print(x)
     print(f"Finished sending notification for book: {book_id}")
